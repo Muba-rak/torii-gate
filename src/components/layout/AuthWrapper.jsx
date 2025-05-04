507357a14a0e0514a0df4c5ee61aa5e11e39185c
Muba-rak <oladapomubarak@gmail.com>
2025-05-04 19:26:29 +0100

    styles: changed auth style

--- src/components/layout/AuthWrapper.jsx
+++ src/components/layout/AuthWrapper.jsx
@@ -3,12 +3,12 @@ import logo from "../../assets/logo.png";
 
 const AuthWrapper = ({ children }) => {
   return (
-    <div className=" bg-[#fbfbfb]">
-      <div className="flex items-center justify-center lg:justify-between h-screen layout bg-[#fbfbfb]">
-        <div className="bg-white rounded-xl h-full overflow-y-scroll flex items-center justify-center">
+    <div className=" bg-[#fbfbfb] ">
+      <div className="flex items-center justify-center lg:justify-between lg:gap-2.5 min-h-screen layout bg-[#fbfbfb] p-2">
+        <div className="bg-white rounded-xl ">
           <div>{children}</div>
         </div>
-        <div className="hidden  lg:flex auth bg-no-repeat bg-cover h-[607px] w-[690px] rounded-2xl  items-end justify-start text-white">
+        <div className="hidden  lg:flex auth bg-no-repeat bg-cover h-[607px] w-[680px] rounded-2xl  items-end justify-start text-white">
           <div className="flex gap-2 items-center mb-5 ml-2.5">
             <img src={logo} alt="logo" />
             <div>

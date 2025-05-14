e793657e960e071c648ccd03b21cba320a9e2f4d
Muba-rak <oladapomubarak@gmail.com>
2025-05-14 15:52:36 +0100

    feat: implemented register workflow

--- src/pages/VerifyEmail.jsx
+++ src/pages/VerifyEmail.jsx
@@ -1,7 +1,50 @@
-import React from "react";
+import { useParams, useNavigate, Link } from "react-router-dom";
+import { axiosInstance } from "../utils/axiosInstance";
+import { useState, useEffect } from "react";
 
 const VerifyEmail = () => {
-  return <div>VerifyEmail</div>;
+  const { token } = useParams();
+  const [errorMsg, setErrorMsg] = useState("");
+  const [status, setStatus] = useState("verifying");
+  const checkToken = async () => {
+    try {
+      const response = await axiosInstance.post(`/auth/verify-email/${token}`, {
+        token,
+      });
+      if (response.status === 200) {
+        setStatus("success");
+      }
+    } catch (error) {
+      setErrorMsg("Email Verification Failed");
+      setStatus("error");
+    }
+  };
+
+  useEffect(() => {
+    checkToken();
+  }, []);
+
+  if (status === "verifying") {
+    return <div>Verifying....</div>;
+  }
+  if (status === "success") {
+    return (
+      <div>
+        <h1>Email Verified Successfully</h1>
+        <Link to="/login">
+          <button>Proceed to Login</button>
+        </Link>
+      </div>
+    );
+  }
+
+  return (
+    <div>
+      <h1>Verification failed</h1>
+      <p>{errorMsg} </p>
+      <button>Resend Verification Email</button>
+    </div>
+  );
 };
 
 export default VerifyEmail;

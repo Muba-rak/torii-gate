ca0d12e58628e91f612032f4d1abef4ddfa0429f
Muba-rak <oladapomubarak@gmail.com>
2025-05-19 13:08:47 +0100

    feat: added login functionality

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -8,6 +8,7 @@ import { useForm } from "react-hook-form";
 import { yupResolver } from "@hookform/resolvers/yup";
 import { loginSchema } from "../utils/formValidator";
 import { axiosInstance } from "../utils/axiosInstance";
+import { useAppContext } from "../hooks/useAppContext";
 
 const Login = () => {
   const [showPassword, setShowPassword] = useState(false);
@@ -15,6 +16,7 @@ const Login = () => {
   const [role, setRole] = useState("tenant");
   const [isSubmitting, setIsSubmitting] = useState(false);
   const redirect = useNavigate();
+  const { login } = useAppContext();
 
   const {
     register,
@@ -24,12 +26,27 @@ const Login = () => {
     resolver: yupResolver(loginSchema),
   });
 
-  const handleLogin = (data) => {
+  const handleLogin = async (data) => {
     setIsSubmitting(true);
     try {
-      console.log("Login Data:", { ...data, role });
+      // console.log("Login Data:", { ...data, role });
+      const { data: mydata } = await axiosInstance.post("/auth/login", {
+        ...data,
+        role,
+      });
+      console.log(mydata);
+      login(mydata.token, mydata.user);
+      if (mydata.user.role === "tenant") {
+        redirect("/home");
+      } else {
+        redirect("/dashboard");
+      }
+      setErrorMessage("");
     } catch (error) {
       console.log(error);
+      setErrorMessage(error?.response?.data?.message || "Login Failed");
+    } finally {
+      setIsSubmitting(false);
     }
   };
 

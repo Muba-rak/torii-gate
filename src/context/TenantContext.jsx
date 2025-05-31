b7ec1f993611a8dd681aa61180a2ed710ae29c2f
Muba-rak <oladapomubarak@gmail.com>
2025-05-31 10:56:13 +0100

    refactor: fixed error codes

---
+++ src/context/TenantContext.jsx
@@ -0,0 +1,52 @@
+import { createContext, useState } from "react";
+import { axiosInstance } from "../utils/axiosInstance";
+import { useAppContext } from "../hooks/useAppContext";
+import { useEffect } from "react";
+
+export const TenantContext = createContext();
+
+const Tenantprovider = ({ children }) => {
+  const [isLoading, setIsLoading] = useState(true);
+  const [properties, setProperties] = useState([]);
+  const [page, setPage] = useState(1);
+  const [totalPage, setTotalPage] = useState(1);
+  const { token } = useAppContext();
+
+  //api call
+
+  const fetchProperties = async () => {
+    if (token) {
+      try {
+        const { data } = await axiosInstance.get(`/property?page=${page}`, {
+          headers: { Authorization: `Bearer ${token}` },
+        });
+        setProperties(data.properties);
+        setPage(data.currentPage);
+        setTotalPage(data.totalPage);
+        setIsLoading(false);
+      } catch (error) {
+        console.log(error);
+      }
+    }
+  };
+
+  useEffect(() => {
+    fetchProperties();
+  }, [token, page]);
+
+  return (
+    <TenantContext.Provider
+      value={{
+        isLoading,
+        properties,
+        page,
+        setPage,
+        totalPage,
+      }}
+    >
+      {children}
+    </TenantContext.Provider>
+  );
+};
+
+export default Tenantprovider;
